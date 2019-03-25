cbb2106034bb8f0dd9ff2cdd8c978070af9b1a08
Nikami <t1_mikami@ukr.net>
2019-03-26 00:24:07 +0200

    Fix dev config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -5,6 +5,9 @@ const webpackCommonConfig = require('./webpack.config');
 
 module.exports = merge(webpackCommonConfig, {
   mode: 'development',
+  output: {
+    publicPath: '/',
+  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.EnvironmentPlugin({ NODE_ENV: 'development' }),
